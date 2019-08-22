0ca572d9f3ec86f56c99310ee559c160d6827778
orangegrove_1955 <orangegrove_1955@hotmail.com>
2019-08-22 17:41:48 +1000

    Added basic setup of project

---
+++ lib/ui/fw.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class FW extends StatefulWidget {
+  @override
+  _FWState createState() => _FWState();
+}
+
+class _FWState extends State<FW> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
